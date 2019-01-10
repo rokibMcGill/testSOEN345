6f9ba42f54b03cc6a36ec37b91afed60522703f0
rokibMcGill <43221015+rokibMcGill@users.noreply.github.com>
2019-01-10 17:28:28 -0500

    First Code

---
+++ testSOEN345/src/testSOEN345/testCodeGit.java
@@ -0,0 +1,10 @@
+package testSOEN345;
+
+public class testCodeGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.print("Code Test in GitHub");
+	}
+
+}
